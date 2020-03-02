751407ab31bf89c369522f92fb6b4f54d5bb2a00
xl <xjc1511012594@163.com>
2020-03-02 20:13:59 +0800

    提交3

---
+++ mqdemo/src/main/java/com/mq/mqdemo/consumer/consumer.java
@@ -0,0 +1,14 @@
+package com.mq.mqdemo.consumer;
+
+/**
+ * @author xulei
+ * @date 2020-2-27 10:30
+ */
+public class consumer {
+
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ mqdemo/src/main/java/com/mq/mqdemo/producer/Producer.java
@@ -0,0 +1,22 @@
+package com.mq.mqdemo.producer;
+
+/**
+ * @author xulei
+ * @date 2020-2-27 10:30
+ */
+public class Producer {
+
+
+    public static void main(String[] args) {
+            //1.创建一个connetcionFactory，并进行配置
+
+            //2.通过连接工厂创建连接
+
+            //3.通过connection创建一个Channel
+
+            //4.通过Channel发送信息
+
+
+
+    }
+}
