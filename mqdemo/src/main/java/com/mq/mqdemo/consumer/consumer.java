611c091cc887502f85fa9a9da6085f734835b27d
xl <xjc1511012594@163.com>
2020-03-03 10:43:23 +0800

    消费者生产者代码

--- mqdemo/src/main/java/com/mq/mqdemo/consumer/consumer.java
+++ mqdemo/src/main/java/com/mq/mqdemo/consumer/consumer.java
@@ -1,5 +1,10 @@
 package com.mq.mqdemo.consumer;
 
+import com.rabbitmq.client.*;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
 /**
  * @author xulei
  * @date 2020-2-27 10:30
@@ -7,7 +12,31 @@ package com.mq.mqdemo.consumer;
 public class consumer {
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, TimeoutException, InterruptedException {
+        //1.创建ConnectionFactory，并进行配置
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+        connectionFactory.setHost("");
+        connectionFactory.setPort(5672);
+        connectionFactory.setVirtualHost("/");
+        //2.通过连接工厂创建连接
+        Connection connection = connectionFactory.newConnection();
+        //3.通过connection创建一个Channel
+        Channel channel = connection.createChannel();
+        //4.声明创建一个队列
+        String queueName="test001";
+        channel.queueDeclare(queueName,true,false,false,null);
+        //5.创建消费者
+        QueueingConsumer queueingConsumer = new QueueingConsumer(channel);
+        //6.设置Channel
+        channel.basicConsume(queueName,true,queueingConsumer);
+        //7.使用while循环轮询消息
+        while (true){
+            //8.获取消息
+            QueueingConsumer.Delivery delivery = queueingConsumer.nextDelivery();
+            String msg = new String(delivery.getBody());
+            System.out.println("消费端："+msg);
+            //Envelope envelope = delivery.getEnvelope();
+        }
 
     }
 
--- mqdemo/src/main/java/com/mq/mqdemo/producer/Producer.java
+++ mqdemo/src/main/java/com/mq/mqdemo/producer/Producer.java
@@ -1,5 +1,12 @@
 package com.mq.mqdemo.producer;
 
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
 /**
  * @author xulei
  * @date 2020-2-27 10:30
@@ -7,16 +14,24 @@ package com.mq.mqdemo.producer;
 public class Producer {
 
 
-    public static void main(String[] args) {
-            //1.创建一个connetcionFactory，并进行配置
-
-            //2.通过连接工厂创建连接
-
-            //3.通过connection创建一个Channel
-
-            //4.通过Channel发送信息
-
-
-
+    public static void main(String[] args) throws IOException, TimeoutException {
+        //1.connectionFactory，并进行配置
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+        connectionFactory.setHost("");
+        connectionFactory.setPort(5672);
+        connectionFactory.setVirtualHost("/");
+        //2.通过连接工厂创建连接
+        Connection connection = connectionFactory.newConnection();
+        //3.通过connection创建一个Channel
+        Channel channel = connection.createChannel();
+        //4.通过Channel发送信息
+        for(int i=0;i<5;i++){
+            String msg="Hello RabbitMQ";
+            channel.basicPublish("","test001",null,msg.getBytes());
+        }
+        //5.关闭相关连接
+        channel.close();
+        connection.close();
     }
+
 }
